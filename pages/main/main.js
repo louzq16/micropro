3b4537d735d075a58a8c38d9824711e2bec0fac0
wutaiqiang <2795823986@qq.com>
2018-12-17 21:13:11 +0800

    wu

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,59 +1,51 @@
+function getRandomColor() {
+  const rgb = []
+  for (let i = 0; i < 3; ++i) {
+    let color = Math.floor(Math.random() * 256).toString(16)
+    color = color.length === 1 ? '0' + color : color
+    rgb.push(color)
+  }
+  return '#' + rgb.join('')
+}
+var doommList = [];
+class Doomm {
+  constructor(text, top, time, color) {
+    this.text = text;
+    this.top = top;
+    this.time = time;
+    this.color = color;
+    this.display = true;
+    let that = this;
+    /*
+    setTimeout(function () {
+      doommList.splice(doommList.indexOf(that), 1);
+      page.setData({
+        doommData: doommList
+      })
+    }, this.time * 100)//定时器动画完成后执行删除
+    */
+  }
+}
 Page(
   {
-  /* 
-  onShareAppMessage() {
-    return {
-      title: 'camera',
-      path: 'page/component/pages/camera/camera'
-    }
-  },
-  */
   data: {
     src: '',
     videoSrc: '',
     position: 'back',
     mode: 'scanCode',
-    result: {}
+    result: {},
+    doommData:[]
   },
   onLoad() {
     this.ctx = wx.createCameraContext()
+  },
+  bindInputBlur(e) {
+      this.inputValue = e.detail.value
+  },
+  bindSendDanmu() {
+      doommList.push(new Doomm(this.inputValue, 40, 10, getRandomColor()));
+      this.setData({
+        doommData: doommList
+      })
   }
-  /*
-  takePhoto() {
-    this.ctx.takePhoto({
-      quality: 'high',
-      success: (res) => {
-        this.setData({
-          src: res.tempImagePath
-        })
-      }
-    })
-  },
-  startRecord() {
-    this.ctx.startRecord({
-      success: () => {
-        console.log('startRecord')
-      }
-    })
-  },
-  stopRecord() {
-    this.ctx.stopRecord({
-      success: (res) => {
-        this.setData({
-          src: res.tempThumbPath,
-          videoSrc: res.tempVideoPath
-        })
-      }
-    })
-  },
-  togglePosition() {
-    this.setData({
-      position: this.data.position === 'front'
-        ? 'back' : 'front'
-    })
-  },
-  error(e) {
-    console.log(e.detail)
-  }
-  */
 })
